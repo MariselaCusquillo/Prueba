a16e66cfecda00bbffd426306f1905bbf1becfa5
mauricio1798 <70928418+mauricio1798@users.noreply.github.com>
2020-09-08 23:33:57 -0500

    Prueba cambio

---
+++ WindowsFormsApp1/WindowsFormsApp1/Prueba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Prueba
+    {
+        public string Name { set; get; }
+        public string Apellido { set; get; }
+
+        public Prueba()
+        {
+
+        }
+
+        public Prueba(string name, string apellido)
+        {
+            Name = name;
+            Apellido = apellido;
+        }
+    }
+}
